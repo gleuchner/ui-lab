75476860f74b0fbb7e44428b6da85fd99b0af13e
JimWest <j1m_w3st@web.de>
2017-03-29 20:33:42 +0200

    added buttons, 3d objects can be changed

--- Exercise 1/js/demo.js
+++ Exercise 1/js/demo.js
@@ -1,5 +1,6 @@
 
 // 3D code partially grabbed from http://dev.opera.com/articles/view/porting-3d-graphics-to-the-web-webgl-intro-part-2/
+const remote = require('electron').remote;
 
 document.addEventListener('DOMContentLoaded', function() {
     if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var camera, scene;
     var webglRenderer;
 
-    var zmesh, geometry;
+    var currentObject;
 
     var mouseX = 0, mouseY = 0;
     var mousemoveX = 0, mousemoveY = 0;
@@ -21,12 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var windowHalfX = window.innerWidth / 2;
     var windowHalfY = window.innerHeight / 2;
 
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
 
-    var closeEl = initCloseBtn();
+    initButons();
 
     init();
     animate();
@@ -36,20 +34,27 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.appendChild( container );
 
         // camera
-        camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 100000 );
-        camera.position.z = 75;
+        camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000 );
+        camera.position.z = 200;
 
         //scene
         scene = new THREE.Scene();
 
-        // lights
-        var ambient = new THREE.AmbientLight( 0xffffff );
-        scene.add( ambient );
+        var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+        var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
+        var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 
-        // more lights
-        var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-        directionalLight.position.set( 0, -70, 100 ).normalize();
-        scene.add( directionalLight );
+        scene.add(skybox);
+
+
+        var dLight = new THREE.DirectionalLight(0xffffff);
+        dLight.position.set(200, 50, 400);
+        scene.add(dLight);
+
+        var pointLight = new THREE.PointLight(0xffffff);
+        pointLight.position.set(0, 100, 200);
+        pointLight.intensity = 1;
+        scene.add(pointLight);
 
         // renderer
         webglRenderer = new THREE.WebGLRenderer();
@@ -57,85 +62,57 @@ document.addEventListener('DOMContentLoaded', function() {
         webglRenderer.domElement.style.position = "relative";
         container.appendChild( webglRenderer.domElement );
 
-        createScene();
-
+        createScene( );
     }
 
-    function initCloseBtn() {
-        var closeEl = document.querySelector(".close");
-        if (closeEl) {
-            closeEl.addEventListener('click', function() {
-                window.close();
+    function initButons() {
+        var button = document.getElementById('cube-button');
+        if (button)
+        {
+            button.addEventListener('click', ()=>{
+                addObject(new THREE.CubeGeometry(100, 100, 100));
+                currentObject.rotation.set(90, 90, 90);
             });
-        };
-        return closeEl;
+        }
+
+        var sphereButton = document.getElementById('sphere-button');
+        if (sphereButton)
+        {
+            sphereButton.addEventListener('click', ()=>{
+                addObject(new THREE.SphereGeometry(50, 16, 16));
+            });
+        }
+
     }
 
-    function createScene( ) {
+    function addObject(geometry)
+    {
+        if (currentObject)
+        {
+            scene.remove(currentObject);
+        }
+
         // create the sphere's material
-        const sphereMaterial =
-            new THREE.MeshPhongMaterial(
-                {
-                    color: 0xCC0000
-                });
+        const material = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
 
-        // Set up the sphere vars
-        const RADIUS = 20;
-        const SEGMENTS = 16;
-        const RINGS = 16;
-
-        // Create a new mesh with
-        // sphere geometry - we will cover
-        // the sphereMaterial next!
-        const sphere = new THREE.Mesh(
-
-            new THREE.SphereGeometry(
-                RADIUS,
-                SEGMENTS,
-                RINGS),
-
-        sphereMaterial);
-        sphere.scale.set( 1, 1, 1 );
-        sphere.position.set( 0, 0, 0 );
+        var object = new THREE.Mesh(geometry, material);
+        object.scale.set( 1, 1, 1 );
+        object.position.set( 0, 0, 0 );
 
         // Finally, add the sphere to the scene.
-        scene.add(sphere);
+        scene.add(object);
+        currentObject = object;
     }
 
-    function onDocumentMouseDown(event) {
-        if (event.target == closeEl) return; // it should deliver click to close button
 
-        document.body.requestPointerLock =
-            document.body.requestPointerLock ||
-            document.body.mozRequestPointerLock ||
-            document.body.webkitRequestPointerLock;
-        document.body.requestPointerLock();
-    }
-
-    function onDocumentMouseUp(event) {
-        document.exitPointerLock =
-            document.exitPointerLock ||
-            document.mozExitPointerLock ||
-            document.webkitExitPointerLock;
-        document.exitPointerLock();
+    function createScene( ) {
+        addObject(new THREE.SphereGeometry(50, 16, 16));
     }
 
     function onDocumentMouseWheel(event) {
         camera.position.z -= event.wheelDelta/120*3;
     }
 
-    function onDocumentMouseMove(event) {
-        mouseX = ( event.clientX - windowHalfX );
-        mouseY = ( event.clientY - windowHalfY );
-        document.pointerLockElement =
-            document.pointerLockElement ||
-            document.mozPointerLockElement ||
-            document.webkitPointerLockElement;
-        if (document.pointerLockElement) {
-            mousemoveX += event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-            mousemoveY += event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-        }
-    }
 
     function animate() {
         requestAnimationFrame( animate );
@@ -143,11 +120,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function render() {
-
-        if (zmesh) {
-            zmesh.rotation.set(-(mouseY + mousemoveY)/windowHalfY + 0,
-                -(mouseX + mousemoveX)/windowHalfX, 0);
-        }
         webglRenderer.render( scene, camera );
     }
 
